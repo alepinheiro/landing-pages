b253f6086a13b46891b3ec05f7bf3ccc4f159de7
alepinheiro <alessandropsbra@gmail.com>
2022-05-27 10:49:36 -0300

    upate link ibramed experience

---
+++ dist/_nuxt/static/1653659342/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002FRentalShop","\u002Festetica-in-sul","\u002FAssistenciaTecnica","\u002FCosmeticON","\u002FHomeRentalMed","\u002Fibramed-experience","\u002FHeccusTurbo"]})
