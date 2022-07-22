63816a2f020cb03d976f16d8140811664d03a0fc
Pinheiro - Site - rentalmed <site@rentalmed.com.br>
2022-07-22 12:50:19 -0300

    update pwa config

---
+++ dist/_nuxt/static/1658504985/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fhtm-training","\u002FCosmeticON","\u002FHomeRentalMed","\u002FAssistenciaTecnica","\u002Festetica-in-sul","\u002FHeccusTurbo","\u002FRentalShop","\u002FAssistenciaTecnica\u002Floja-integrada","\u002Fibramed-experience","\u002Fworkshop-emily-santos","\u002Fblackout"]})
